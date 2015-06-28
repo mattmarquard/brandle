341c727028a9e6a28bda527f23702c26983e170b
mattmarquard <matt@thevertigo.com>
2015-06-28 16:28:21 -0700

    fixes

--- public/javascripts/brandleApp.js
+++ public/javascripts/brandleApp.js
@@ -9,6 +9,11 @@ function($stateProvider, $urlRouterProvider) {
       url: '/home',
       templateUrl: '/home.html',
       controller: 'MainCtrl'
+    })
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
     });
 
   $urlRouterProvider.otherwise('home');
@@ -20,24 +25,47 @@ function($stateProvider, $urlRouterProvider) {
   return o;
 }])
 .controller('MainCtrl', [
-'$scope',
-'posts',
-function($scope, posts){
-    $scope.test = 'Hello world!';
-    $scope.posts = posts.posts;
+    '$scope',
+    'posts',
+    function($scope, posts){
+	$scope.test = 'Hello world!';
+	$scope.posts = posts.posts;
 
-    $scope.addPost = function(){
-        if(!$scope.title || $scope.title === '') { return; }
-        $scope.posts.push({
-	    title: $scope.title,
-	    link: $scope.link,
+	$scope.addPost = function(){
+	    if(!$scope.title || $scope.title === '') { return; }
+	    $scope.posts.push({
+	        title: $scope.title,
+	        link: $scope.link,
+	        upvotes: 0,
+	        comments: [
+		    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+		    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+		]
+	    });
+	    $scope.title = '';
+	    $scope.link = '';
+	};
+	$scope.incrementUpvotes = function(post) {
+	    post.upvotes += 1;
+	};
+    }
+])
+.controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function($scope, $stateParams, posts){
+	$scope.post = posts.posts[$stateParams.id];
+
+	$scope.addComment = function(){
+	  if($scope.body === '') { return; }
+	  $scope.post.comments.push({
+	    body: $scope.body,
+	    author: 'user',
 	    upvotes: 0
-        });
-        $scope.title = '';
-        $scope.link = '';
-    };
-    $scope.incrementUpvotes = function(post) {
-	post.upvotes += 1;
-    };
-}]);
+	  });
+	  $scope.body = '';
+	};
+    }
+]);
 
