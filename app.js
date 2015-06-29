4fdd674eaab64edb2f6093c36d8ab019110a4033
mattmarquard <matt@thevertigo.com>
2015-06-29 14:36:19 -0700

    few remaining login issues

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var passport = require('passport');
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
+require('./config/passport');
 
 mongoose.connect('mongodb://localhost/news');
 
@@ -14,6 +17,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.use(passport.initialize());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
